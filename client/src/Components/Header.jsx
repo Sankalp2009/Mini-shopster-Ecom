288f8afaf1318f2be444d7d76e79aaee943a0a61
sankalp patel <sankalppatel38@gmail.com>
2026-07-04 00:56:52 +0530

    Display_productGrid

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
-
+import Search from './Search'
+import { ShoppingCart } from 'lucide-react'
+import { Link } from 'react-router'
 function Header() {
   return (
-    <div>Header</div>
+    <header className="Header-Top">
+      <div className="Header-inner">
+        <div className="Header-img">
+          <Link to="/">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSrC_Ks-lEUuoQQeZEPHa6L5RSa-p3L0W8qdj56kR_2Q&s=10"
+            alt="Logo"
+          />
+          </Link>
+        </div>
+
+        <div className="Header-search">
+          <div>
+            <Link to="/">Home</Link>
+          </div>
+          <Search />
+        </div>
+
+        <div className="Header-actions">
+          <div>
+            <Link to="/cart">
+              <ShoppingCart />
+            </Link>
+          </div>
+          <div>
+            <Link to="/login">
+              <button className="login-btn">Login</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
---
+++ client/src/Components/Search.jsx
@@ -0,0 +1,11 @@
+
+
+function Search() {
+  return (
+    <div>
+      <input type="text" name='Search' placeholder='Enter Search...' />
+    </div>
+  )
+}
+
+export default Search
